c7c852265fb6bbc47323383e795331b60b058acb
Eunice Sarah Siregar <13521013@mahasiswa.itb.ac.id>
2024-03-12 17:56:01 +0700

    feat: add navbar

--- bondoman/app/src/main/java/com/example/bondoman/MainActivity.kt
+++ bondoman/app/src/main/java/com/example/bondoman/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.bondoman
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bondoman.databinding.ActivityMainBinding
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+               R.id.scan_cam ->replaceFragment(ScanPage())
+                R.id.graph ->replaceFragment(GraphPage())
+                R.id.settings ->replaceFragment(SettingsPage())
+                R.id.bill ->replaceFragment(TransactionPage())
+
+            }
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
